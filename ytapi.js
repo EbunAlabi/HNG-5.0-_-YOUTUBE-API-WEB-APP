90fbe70119d302198c062cdc8c54410fbcb4e011
EbunAlabi <30610048+EbunAlabi@users.noreply.github.com>
2019-03-31 23:35:31 +0100

    first commit, after local commit

---
+++ ytapi.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+var key  = 'AIzaSyC15WjeVOuk1heOb62Tpcb6kSg8Fw1dwbE' ; 
+var playlistId = 'PLpMlJqmSgXXJ8hJe9AuGN-KdprchYU2d9';
+var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+
+
+var options = {
+    part: 'snippet',
+    key: key,
+    maxResults: 20,
+    playlistId: playlistId
+}
+
+
+
+loadvids();
+
+
+function loadvids(){
+    $.getJSON(URL, options, function(data){
+        console.log(data);
+      var id = data.items[0].snippet.resourceId.videoId;
+        mainVid(id);
+        resultsLoop(data);
+    })
+    
+
+}
+
+
+
+function mainVid(id){
+    $('#video').html(`            <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `);
+}
+
+
+function resultsLoop(data){
+
+
+$.each(data.items, function(i,item){
+    var thumb = item.snippet.thumbnails.medium.url;
+    var title = item.snippet.title;
+    var desc = item.snippet.description.substring(0,100 );
+   var vid = item.snippet.resourceId.videoId;
+    $('main').append(`
+    
+    <article class="item" data-key="${vid}">
+    <img src="${thumb}" alt="thumbnail" class="thumb">
+    <div class="details">
+        <h4>${title}</h4>
+        <p>${desc}</p>
+    </div>
+</article>
+
+    `);
+
+
+
+
+
+
+});
+$('main').on('click', 'article', function(){
+   
+    var id = $(this).attr('data-key');
+    mainVid(id);
+    
+    
+    });
+   
+}
+
+});
